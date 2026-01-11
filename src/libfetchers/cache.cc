nix-windows/nix

Convert more end exes

src/libfetchers/cache.cc
@@ -36,10 +36,10 @@ struct CacheImpl : Cache
     {
         auto state(_state.lock());
 
-        auto dbPath = getCacheDir() + "/fetcher-cache-v2.sqlite";
-        createDirs(dirOf(dbPath));
+        auto dbPath = getCacheDir() / "fetcher-cache-v2.sqlite";
+        std::filesystem::create_directories(dbPath.parent_path());
 
-        state->db = SQLite(dbPath);
+        state->db = SQLite(dbPath.string());
         state->db.isCache();
         state->db.exec(schema);
 

andrewgazelka/nix-1

src/libfetchers/cache.cc
@@ -38,10 +38,10 @@ struct CacheImpl : Cache
     {
         auto state(_state.lock());
 
-        auto dbPath = (getCacheDir() / "fetcher-cache-v4.sqlite").string();
-        createDirs(dirOf(dbPath));
+        auto dbPath = getCacheDir() / "fetcher-cache-v4.sqlite";
+        createDirs(dbPath.parent_path());
 
-        state->db = SQLite(dbPath);
+        state->db = SQLite(dbPath.string());
         state->db.isCache();
         state->db.exec(schema);
 